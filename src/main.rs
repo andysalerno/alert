9132e211bf3e309176188903d18964645db3ec12
andy salerno <andysalerno@gmail.com>
2018-07-04 17:34:35 -0700

    improve windows integration

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 #[cfg(target_os = "macos")]
 mod macosalert;
+#[cfg(target_os = "macos")]
 use macosalert::alert;
 
 #[cfg(windows)]
 mod winalert;
+
+#[cfg(windows)]
+use winalert::alert;
 
 fn main() {
     alert();
--- src/winalert.rs
+++ src/winalert.rs
@@ -10,19 +10,31 @@ extern crate user32;
 #[cfg(windows)]
 use std::ptr;
 
-pub fn get_console_window() {}
+use self::winapi::HWND;
 
-fn alert() {
-    use winapi::winuser::FLASHWINFO;
+pub fn alert() {
+    let console_window_result = get_console_window();
 
-    let window = unsafe { kernel32::GetConsoleWindow() };
-
-    if window == ptr::null_mut() {
-        return;
+    if let Ok(console_window) = console_window_result {
+        flash_window(console_window);
+    } else {
+        panic!("Unable to find console window.");
     }
 }
 
-pub fn flash_window(window: *mut HWND) {
+fn get_console_window() -> Result<HWND, ()> {
+    let window = unsafe { kernel32::GetConsoleWindow() };
+
+    if window == ptr::null_mut() {
+        return Err(());
+    }
+
+    Ok(window)
+}
+
+fn flash_window(window: HWND) {
+    use self::winapi::winuser::FLASHWINFO;
+
     // https://docs.microsoft.com/en-us/windows/desktop/api/winuser/ns-winuser-flashwinfo
     const FLASH_COUNT: u32 = 3;
     const DEFAULT_TIMEOUT: u32 = 0;
@@ -30,7 +42,7 @@ pub fn flash_window(window: *mut HWND) {
 
     let mut flash_info = FLASHWINFO {
         hwnd: window,
-        cbSize: std::mem::size_of::<FLASHWINFO>() as u32,
+        cbSize: ::std::mem::size_of::<FLASHWINFO>() as u32,
         dwFlags: FLASH_UNTIL_FOCUSED,
         dwTimeout: DEFAULT_TIMEOUT,
         uCount: FLASH_COUNT,
